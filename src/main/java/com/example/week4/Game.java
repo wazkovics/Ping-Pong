0021dc4e1ea8615613637483ca7b17a611e02d67
wazkovics <161438972+wazkovics@users.noreply.github.com>
2024-03-12 09:52:25 +0000

    Comments completed in Game.java

--- src/main/java/com/example/week4/Game.java
+++ src/main/java/com/example/week4/Game.java
@@ -22,6 +22,9 @@ public class Game implements Resizable {
 
     private int gamedifficulty;
 
+    /**
+     * Creates a game object with 2 new players and a ball, sets the default window size
+     */
     public Game(){
         dimensionX = 600;
         dimensionY = 450;
@@ -33,30 +36,59 @@ public class Game implements Resizable {
     }
 
     //Getters
+
+    /**
+     * Gets the player 1 from the game
+     * @return Return the player 1 object from the game
+     */
     public Player getPlayer1() {
         return player1;
     }
 
+    /**
+     * Gets the player 1 from the game
+     * @return Return the player 2 object from the game
+     */
     public Player getPlayer2() {
         return player2;
     }
 
+    /**
+     * Get the ball that is currently being used by the game
+     * @return Return the ball object that is currently being used by the game
+     */
     public Ball getBall() {
         return ball;
     }
 
+    /**
+     * Get the current window width
+     * @return Return the current window width
+     */
     public double getDimensionX() {
         return dimensionX;
     }
 
+    /**
+     * Get the current window height
+     * @return Return the current window height
+     */
     public double getDimensionY() {
         return dimensionY;
     }
 
+    /**
+     * Get the game ending score from the game
+     * @return Return the game ending score
+     */
     public int getGameendingscr() {
         return gameendingscr;
     }
 
+    /**
+     * Get the game difficult from the game
+     * @return Return the game difficulty from the game
+     */
     public int getGamedifficulty() {
         return gamedifficulty;
     }
@@ -65,46 +97,68 @@ public class Game implements Resizable {
 
 
     //Setters
-    public void setPlayer1(Player player1) {
-        this.player1 = player1;
-    }
+//    public void setPlayer1(Player player1) {
+//        this.player1 = player1;
+//    }
+//
+//    public void setPlayer2(Player player2) {
+//        this.player2 = player2;
+//    }
+//
+//    public void setBall(Ball ball) {
+//        this.ball = ball;
+//    }
 
-    public void setPlayer2(Player player2) {
-        this.player2 = player2;
-    }
-
-    public void setBall(Ball ball) {
-        this.ball = ball;
-    }
-
+    /**
+     * Set the width of the game window
+     * @param dimensionX New window width to be applied
+     */
     public void setDimensionX(double dimensionX) {
         this.dimensionX = dimensionX;
     }
 
+    /**
+     * Set the height of the game window
+     * @param dimensionY New window height to be applied
+     */
     public void setDimensionY(double dimensionY) {
         this.dimensionY = dimensionY;
     }
 
 
-
+    /**
+     * Increment the game ending score by one
+     */
     public void setGameendingscrUp() {
         this.gameendingscr++;
     }
+
+    /**
+     * Decrement the game ending score by one
+     */
     public void setGameendingscrDown() {
         this.gameendingscr--;
     }
 
+    /**
+     * Increment the game difficulty by one
+     */
     public void setGamedifficultyUp() {
         this.gamedifficulty++;
     }
 
+    /**
+     * Decrement the game difficulty by one
+     */
     public void setGamedifficultyDown() {
         this.gamedifficulty--;
     }
 
 
-
-
+    /**
+     * Resize the Y dimension of the game objects whenever the width is increased by the user
+     * @param factor The multiplier by which the game window has increased/decreased from previous value, this is used to then increase the size of game objects
+     */
     @Override
     public void resizeX(double factor) {
         player1.getRacket().resizeX(factor);
@@ -114,6 +168,10 @@ public class Game implements Resizable {
         player2.resizeX(factor);
     }
 
+    /**
+     * Resize the Y dimension of the game objects whenever the height is increased by the user
+     * @param factor The multiplier by which the game window has increased/decreased from previous value, this is used to then increase the size of game objects
+     */
     @Override
     public void resizeY(double factor) {
         player1.getRacket().resizeY(factor);
